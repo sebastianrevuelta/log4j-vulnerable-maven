04ca1f695d234fb14356b110c09c8a3360659573
sebastianrevuelta <sebastianrevuelta@gmail.com>
2023-02-02 13:38:26 +0100

    add new indentation

--- src/main/java/HelloLog.java
+++ src/main/java/HelloLog.java
@@ -14,13 +14,13 @@ public class HelloLog {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        String userInput = "${jndi:http://localhost/AAAA/BBBB}";
+      String userInput = "${jndi:http://localhost/AAAA/BBBB}";
 
-        // passing user input into the logger, it a log4j critical vuln
-        logger.info("Test: "+userInput);
+      // passing user input into the logger, it a log4j critical vuln
+      logger.info("Test: "+userInput);
 
-        // %m{nolookups} has no effect for the following line
-        logger.printf(Level.INFO,"Test: %s", userInput);
+      // %m{nolookups} has no effect for the following line
+      logger.printf(Level.INFO,"Test: %s", userInput);
     }
 
    public static byte[] bad1(String password) throws NoSuchAlgorithmException {
